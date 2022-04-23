e1bf95c8781b24c33b06ddc837787dc0109ad04c
Alessio Pascucci <feeder81@gmail.com>
2022-04-23 14:02:31 +0200

    Test BloomFilters download
    Write a test to validate the  Bloom Filter download facility then used by PROXY

--- server_test.go
+++ server_test.go
@@ -3,19 +3,27 @@ package brain
 import (
 	"bytes"
 	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/Ringloop/pisec/elastic"
+	"github.com/bits-and-blooms/bloom/v3"
 	"github.com/stretchr/testify/require"
 )
 
+var testDenyElements = map[string]struct{}{"google.com": {}, "evil.com": {}, "evil.it": {}}
+var testMissingDenyElements = map[string]struct{}{"google.it": {}, "evil.net": {}, "amNotEvil.com": {}}
+
+var testIndicators = []Indicator{
+	{"google.com", "", 99},
+	{"evil.com", "127.0.0.1", 50},
+	{"evil.it", "127.0.0.1", 50},
+}
+
 func TestInsertAndDownload(t *testing.T) {
 	//given
-
-	var testDenyElements = map[string]struct{}{"google.com": {}, "evil.com": {}, "evil.it": {}}
-
 	repo, err := elastic.NewDefaultClient()
 	if err != nil {
 		panic(err)
@@ -26,12 +34,8 @@ func TestInsertAndDownload(t *testing.T) {
 
 	//when (sendind crawler request)
 	test := UrlsBulkRequest{
-		Source: "test-source",
-		Indicators: []Indicator{
-			{"google.com", "", 99},
-			{"evil.com", "127.0.0.1", 50},
-			{"evil.it", "127.0.0.1", 50},
-		},
+		Source:     "test-source",
+		Indicators: testIndicators,
 	}
 	var buf bytes.Buffer
 	err = json.NewEncoder(&buf).Encode(test)
@@ -54,4 +58,55 @@ func TestInsertAndDownload(t *testing.T) {
 	}
 
 	require.Empty(t, testDenyElements)
+}
+
+func TestInsertAndDownloadIndicator(t *testing.T) {
+	//given
+	repo, err := elastic.NewDefaultClient()
+	if err != nil {
+		panic(err)
+	}
+	repo.Delete("denylist")
+	server := NewTestServer()
+	server.routes()
+
+	//when (sendind crawler request)
+	test := UrlsBulkRequest{
+		Source:     "test-source",
+		Indicators: testIndicators,
+	}
+	var buf bytes.Buffer
+	err = json.NewEncoder(&buf).Encode(test)
+	if err != nil {
+		panic(err)
+	}
+	_, _ = http.NewRequest(http.MethodPost, "/api/v1/indicator/url", &buf)
+
+	insertReq := httptest.NewRequest("POST", "/api/v1/indicator/url", &buf)
+	insertRec := httptest.NewRecorder()
+	server.router.ServeHTTP(insertRec, insertReq)
+
+	downloadIndicatorsReq := httptest.NewRequest("GET", "/api/v1/indicators", nil)
+	downloadIndicatorsRec := httptest.NewRecorder()
+	server.router.ServeHTTP(downloadIndicatorsRec, downloadIndicatorsReq)
+
+	if want, got := http.StatusOK, downloadIndicatorsRec.Result().StatusCode; want != got {
+		t.Fatalf("expected a %d, instead got: %d", want, got)
+	}
+
+	res := downloadIndicatorsRec.Result()
+	defer res.Body.Close()
+	jsonRes, err := ioutil.ReadAll(res.Body)
+
+	bFilter := bloom.NewWithEstimates(1000000, 0.01)
+	bFilter.UnmarshalJSON(jsonRes)
+
+	for key, _ := range testDenyElements {
+		require.True(t, bFilter.TestString(key))
+	}
+
+	for key, _ := range testMissingDenyElements {
+		require.False(t, bFilter.TestString(key))
+	}
+
 }
